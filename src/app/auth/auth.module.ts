b7e07632598071568f407e599e39c4fb667e6027
Fernando Nicolas <fersanti2896@gmail.com>
2022-03-19 20:59:26 -0600

    Validando campo nombre con un regex en registro.component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { RegistroComponent } from './pages/registro/registro.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 @NgModule({
   declarations: [
-    RegistroComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AuthRoutingModule
   ]
 })
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RegistroComponent implements OnInit {
+  /* Temporalmente */
+  regexFullName: string = '([a-zA-Z]+) ([a-zA-Z]+)';
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ '', [ Validators.required, Validators.pattern(this.regexFullName) ]]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  campoValido(campo: string) {
+    return this.miFormulario.get(campo)?.invalid
+           && this.miFormulario.get(campo)?.touched;
+  }
+
+  submitFormulario() {
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.markAllAsTouched();
+  }
 }
